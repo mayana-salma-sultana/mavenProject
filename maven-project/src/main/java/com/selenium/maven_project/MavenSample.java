7af3b888f6f8d3749290988af3a0e41560da7b62
ITSUPPORT <ITSUPPORT@MDTR22.mlad.in>
2019-07-04 16:40:57 +0530

    firstcommit

---
+++ maven-project/src/main/java/com/selenium/maven_project/MavenSample.java
@@ -0,0 +1,36 @@
+package com.selenium.maven_project;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MavenSample {
+
+
+	public static void main(String[] args) throws InterruptedException 
+	
+	
+	
+	
+	{
+		
+		 WebDriver driver=new ChromeDriver();
+	
+
+		 
+		 driver.get("http://connect.maveric-systems.com/index.php/site/login");
+		WebElement username = driver.findElement(By.xpath("//input[@id='LoginForm_username']"));
+		username.sendKeys("salmasultanam");
+		
+		
+	WebElement password = driver.findElement(By.xpath("//input[@id='LoginForm_password']"));
+password.sendKeys("");
+
+Thread.sleep(2000);
+		
+		
+	
+	}
+
+}
